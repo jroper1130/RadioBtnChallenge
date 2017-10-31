1fa3e2af0e7b37df59c42b06443bb264e80d9a81
jroper1130 <warbeast78@hotmail.com>
2017-10-31 02:46:37 -0700

    Add files via upload

---
+++ ConditionalRadioButton/ConditionalRadioButton/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ConditionalRadioButton.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>What is your favorite movie?</h1>
+        <div>
+            <asp:RadioButton ID="RadioButton1" runat="server" GroupName="movies" Text="Lord of the Rings" />
+            <br />
+            <asp:RadioButton ID="RadioButton2" runat="server" GroupName="movies" Text="God Father" />
+            <br />
+            <asp:RadioButton ID="RadioButton3" runat="server" GroupName="movies" Text="Alien" />
+            <br />
+            <asp:RadioButton ID="RadioButton4" runat="server" GroupName="movies" Text="Gladiator" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Ok" />
+            <br />
+            <br />
+            <asp:Image ID="Image1" runat="server" />
+&nbsp;<br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ConditionalRadioButton/ConditionalRadioButton/Default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConditionalRadioButton
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (RadioButton1.Checked)
+            {
+                Label1.Text = "Of course it is! The are mine aswell, I recomend the books.";
+            }
+            else if (RadioButton2.Checked)
+            {
+                Label1.Text = "Well they do say it is the best movie of all time!";
+            }
+            else if (RadioButton3.Checked)
+            {
+                Label1.Text = "Helped define a genre and produced the best monster in film, not a bad pick!";
+            }
+            else if (RadioButton4.Checked)
+            {
+                Label1.Text = "An over all amazing movie, the great actors made it that much better!";
+            }
+            else
+            {
+                Label1.Text = "Well? What is it? Or do you not like these?";
+            }/*
+            if (RadioButton1.Checked)
+            {
+                Image1.ImageUrl = "Ringstrilogyposter.jpg";
+            }
+            else if (RadioButton2.Checked)
+            {
+                Image1.ImageUrl = "The_Godfather,_The_Game.jpg";
+            }
+            else if (RadioButton3.Checked)
+            {
+                Image1.ImageUrl = "Alien_2XLP_FC_a5c27829-ef23-4f62-8b56-dc844f53df9d_1024x1024.jpg";
+            }
+            else if (RadioButton4.Checked)
+            {
+                Image1.ImageUrl = "5vZw7ltCKI0JiOYTtRxaIC3DX0e.jpg";
+            }
+            else
+            {
+                Image1.ImageUrl = "C8qDcUcWsAAHGK_.jpg";
+            }*/
+        }
+    }
+        
+}
+//Image1.ImageUrl= "Ringstrilogyposter.jpg";
+//Image1.ImageUrl = "The_Godfather,_The_Game.jpg";
+//Image1.ImageUrl = "Alien_2XLP_FC_a5c27829-ef23-4f62-8b56-dc844f53df9d_1024x1024.jpg";
+//Image1.ImageUrl = "5vZw7ltCKI0JiOYTtRxaIC3DX0e.jpg";
+//Image1.ImageUrl = "C8qDcUcWsAAHGK_.jpg";
